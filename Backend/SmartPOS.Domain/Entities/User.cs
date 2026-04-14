af363890d211b8b4508d24a3adbb054248badcf1
Meet Pandya <mitpandya22@gmail.com>
2026-04-14 23:07:02 +0530

    SPP-201 added user and role entities.

---
+++ Backend/SmartPOS.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPOS.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public bool IsActive { get; set; } = true;
+        public Role? Role { get; set; }
+    }
+}
